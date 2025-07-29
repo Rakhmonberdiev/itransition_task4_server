508910ffc475b2c3cf2af50c42a172408846097d
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2025-07-29 08:32:14 +0500

    feat: обновление LastLoginAt при логине
    - Добавлен импорт пространства имен `Microsoft.EntityFrameworkCore`
      в файл `LoginEndpoint.cs`.
    - В методе обработки логина добавлена логика обновления свойства
      `LastLoginAt` для пользователя. После успешной проверки пароля
      поле обновляется в базе данных с использованием метода
      `ExecuteUpdateAsync`, устанавливая значение `DateTime.UtcNow`.

--- Endpoints/Auth/LoginEndpoint.cs
+++ Endpoints/Auth/LoginEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿using itransition_task4_server.Data.Entities;
 using itransition_task4_server.Endpoints.Auth.DTOs;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace itransition_task4_server.Endpoints.Auth
 {
@@ -16,7 +17,8 @@ namespace itransition_task4_server.Endpoints.Auth
                 var result = await signInManager.CheckPasswordSignInAsync(user, request.Password, false);
                 if (!result.Succeeded)
                     return Results.Unauthorized();
-
+                await userManager.Users.Where(u => u.Id == user.Id)
+                    .ExecuteUpdateAsync(u => u.SetProperty(x => x.LastLoginAt, DateTime.UtcNow));
                 await signInManager.SignInAsync(user, request.isPersistent);
                 return Results.Ok(new AuthResponse(user.Email!));
             }).WithName("Login");
